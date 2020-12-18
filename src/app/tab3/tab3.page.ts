1e80700630d5e65d0ed89146696e2cea60f480fe
Qistina-Everleagues <qistina.mahmud@everleagues.com>
2020-12-18 17:18:18 +0800

    Add Google Maps 50% finish

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ViewChild, ElementRef } from "@angular/core";
+
+declare var google : any; 
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +10,89 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  map: any;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
+  infoWindows: any = [];
+  markers: any = [
+    {
+      title: "National Art Gallery",
+      latitude: "-17.824991",
+      longitude: "31.049295"
+    },
+    {
+      title: "West End Hospital",
+      latitude: "-17.820987",
+      longitude: "31.039682"
+    },
+    {
+      title: "Dominician Convent School",
+      latitude: "-17.822647",
+      longitude: "31.052042"
+    },
+
+    {
+      title: "Chop Chop Brazilian Steakhouse",
+      latitude: "-17.819460",
+      longitude: "31.053844"
+    }
+  ]
+
   constructor() {}
 
+  ionViewDidEnter() {
+    this.showMap();
+  }
+
+  addMarkersToMap(markers) {
+    for (let marker of markers) {
+      let position = new google.maps.LatLng(marker.latitude, marker.longitude);
+      let mapMarker = new google.maps.Marker ({
+        position: position,
+        title: marker.title,
+        latitude: marker.latitude,
+        longitude: marker.longitude
+      });
+
+      mapMarker.setMap(this.map);
+      this.addInfoWindowToMarker(mapMarker);
+    }
+  }
+
+  addInfoWindowToMarker(marker) {
+    let infoWindowContent = '<div id="content">' +
+                            '<h2 id="firstHeading" class"firstHeading">' + marker.title + '</h2>' +
+                            '<p>Latitude: ' + marker.latitude + '</p>' +
+                            '<p>Longitude: ' + marker.longitude + '</p>' +
+                            '</div>';
+
+    let infoWindow = new google.maps.infowWindow ({
+      content: infoWindowContent
+    });
+
+    marker.assListener('click', () => {
+      this.closeAllInfoWindows();
+      infoWindow.open(this.map, marker);
+    });
+
+    this.infoWindows.push(infoWindow);
+  }
+
+  closeAllInfoWindows() {
+    for(let window of this.infoWindows) {
+      window.close();
+    }
+  }
+
+  showMap() {
+    const location = new google.maps.LatLng(-17.824858, 31.053028);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    this.addMarkersToMap(this.markers);
+  }
 }
