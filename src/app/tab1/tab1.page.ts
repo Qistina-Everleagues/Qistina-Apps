c86515c032bab341a7eeefc15ddf9d68c26f683a
Qistina-Everleagues <qistina.mahmud@everleagues.com>
2020-12-21 14:57:30 +0800

    Add http get & avatar

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  friends: any = [];
+
+  runHttp() {
+    this.http.get('https://demo7523367.mockable.io/')
+      .subscribe(data => {
+        console.log(data);
+        this.friends = data;
+      });
+  }
 
 }
