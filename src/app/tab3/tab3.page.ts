9c39fcb08bc5bbf0421846ee5afc157373ebc808
Qistina-Everleagues <qistina.mahmud@everleagues.com>
2020-12-21 11:28:11 +0800

    Add navigation and fix bugs

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ViewChild, ElementRef } from "@angular/core";
 
-declare var google : any; 
+declare var google: any; 
 
 @Component({
   selector: 'app-tab3',
@@ -17,27 +17,31 @@ export class Tab3Page {
   infoWindows: any = [];
   markers: any = [
     {
-      title: "National Art Gallery",
-      latitude: "-17.824991",
-      longitude: "31.049295"
+      title: "Masjid Taman Sri Lambak",
+      latitude: "1.9578",
+      longitude: "103.3844"
     },
     {
-      title: "West End Hospital",
-      latitude: "-17.820987",
-      longitude: "31.039682"
+        title: "Gunung Lambak",
+        latitude: "2.0275",
+        longitude: "103.3575"
     },
     {
-      title: "Dominician Convent School",
-      latitude: "-17.822647",
-      longitude: "31.052042"
+        title: "Kluang Mall",
+        latitude: "2.0401",
+        longitude: "103.3206"
     },
-
     {
-      title: "Chop Chop Brazilian Steakhouse",
-      latitude: "-17.819460",
-      longitude: "31.053844"
+        title: "Watsons",
+        latitude: "1.8639",
+        longitude: "102.9621"
+    },
+    {
+        title: "Kluang Art Street",
+        latitude: "2.0326",
+        longitude: "103.3197"
     }
-  ]
+  ];
 
   constructor() {}
 
@@ -48,7 +52,7 @@ export class Tab3Page {
   addMarkersToMap(markers) {
     for (let marker of markers) {
       let position = new google.maps.LatLng(marker.latitude, marker.longitude);
-      let mapMarker = new google.maps.Marker ({
+      let mapMarker = new google.maps.Marker({
         position: position,
         title: marker.title,
         latitude: marker.latitude,
@@ -62,20 +66,29 @@ export class Tab3Page {
 
   addInfoWindowToMarker(marker) {
     let infoWindowContent = '<div id="content">' +
-                            '<h2 id="firstHeading" class"firstHeading">' + marker.title + '</h2>' +
-                            '<p>Latitude: ' + marker.latitude + '</p>' +
-                            '<p>Longitude: ' + marker.longitude + '</p>' +
+                              '<h2 id="firstHeading" class"firstHeading">' + marker.title + '</h2>' +
+                              '<p>Latitude: ' + marker.latitude + '</p>' +
+                              '<p>Longitude: ' + marker.longitude + '</p>' +
+                              '<ion-button id="navigate">Navigate</ion-button>' +
                             '</div>';
 
-    let infoWindow = new google.maps.infowWindow ({
+    let infoWindow = new google.maps.InfoWindow({
       content: infoWindowContent
     });
 
-    marker.assListener('click', () => {
+    marker.addListener('click', () => {
       this.closeAllInfoWindows();
       infoWindow.open(this.map, marker);
-    });
 
+      google.maps.event.addListenerOnce(infoWindow, 'domready', () => {
+        document.getElementById('navigate').addEventListener('click', () => {
+          console.log('navigate button clicked!');
+          // code to navigate using google maps app
+          window.open('https://www.google.com/maps/dir/?api=1&destination=' + marker.latitude + ',' + marker.longitude);
+        });
+      });
+
+    });
     this.infoWindows.push(infoWindow);
   }
 
@@ -86,7 +99,7 @@ export class Tab3Page {
   }
 
   showMap() {
-    const location = new google.maps.LatLng(-17.824858, 31.053028);
+    const location = new google.maps.LatLng(2.0301, 103.3185);
     const options = {
       center: location,
       zoom: 15,
@@ -95,4 +108,5 @@ export class Tab3Page {
     this.map = new google.maps.Map(this.mapRef.nativeElement, options);
     this.addMarkersToMap(this.markers);
   }
+
 }
